a73b9d72c3e62d19e70740f3121798b5e385a343
Johannes Koeppern <johannes.koeppern@gmail.com>
2023-05-17 16:18:05 +0200

    dataset is created

--- polynomial_regression_demo/src/data/make_dataset.py
+++ polynomial_regression_demo/src/data/make_dataset.py
@@ -1,30 +1,76 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+# 2023-05-17, J. Köppern
+# Generate noisy data with outlyers
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# %%
+# Interpolation points through which the polynomial is to pass
+x = np.array([0, 50, 100, 200, 250, 290, 300])
+y = np.array([0, 50, 110, 170, 130, 120, 120]) / 10
+
+def plot_xy(x, y):
+    plt.plot(x, y)
+
+    plt.title("Interpolation points")
+
+    plt.grid(True)
+
+plot_xy(x, y)
+# %%
+n_degree = len(x)
+
+poly = np.poly1d(np.polyfit(x, y, n_degree))
+
+x_poly = np.linspace(min(x), max(x), 1000)
+
+y_poly = poly(x_poly)
+
+plot_xy(x_poly, y_poly)
+# %%
+# Add noise and outlyers
+noise_percentag = 0.1
+noise_magnitude = noise_percentag * np.abs(y_poly)
+
+noise = np.random.uniform(low=-noise_magnitude, 
+                          high=noise_magnitude, 
+                          size=y_poly.shape)
+
+y_poly_with_noise = y_poly + noise
+
+# Apply the lower bound of zero
+y_poly_with_noise = np.maximum(y_poly_with_noise, 0)
+
+plot_xy(x_poly, y_poly_with_noise)
+# %%
+# Add outliers
+n_outliers = 5
+
+y_poly_with_noise_outliers = y_poly_with_noise
+
+outlier_indices = [np.random.randint(0, len(y_poly_with_noise)) for i in range(n_outliers)]
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+y_poly_with_noise_outliers[outlier_indices] = 0
+
+plot_xy(x_poly, y_poly_with_noise_outliers)
+# %%
+# Create CSV
+# Assuming x_poly and y_poly_with_noise_outliers are your arrays
+data = {'x': x_poly, 'y': y_poly_with_noise_outliers}
+
+# Create a DataFrame
+df = pd.DataFrame(data)
+
+#df.to_csv('./../../data/raw/raw_data.csv', index=False)
+df.to_csv('./data/raw/raw_data.csv', index=False)
+
+import os
+
+current_path = os.getcwd()
+print(current_path)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+# %%
